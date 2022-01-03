c5469b9881e70a795d3d0dae35a8d713ac75ddd6
Khai Nguyen <khainguyenstk@gmail.com>
2022-01-03 12:23:32 +0700

    Update part2 exercise thephonebook step6

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,36 +1,24 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import Add from './Components/Add'
 import Display from './Components/Display'
 import Filter from './Components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '39-44-5323523',
-      id: 1
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-5323523',
-      id: 2
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345',
-      id: 3
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-      id: 4
-    }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [personsToDisplay, setPersonsToDisplay] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setPersonsToDisplay(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
