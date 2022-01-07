6595a44960e8cf03ccea6530986ddc9fd2ab32d7
Khai Nguyen <khainguyenstk@gmail.com>
2022-01-07 10:17:19 +0700

    Update part3 exercise phonebookbackend 3.11 full stack

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -12,6 +13,8 @@ morgan.token('postData', (request, response) => {
     }
 })
 
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan((tokens, request, response) => {
@@ -109,7 +112,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
