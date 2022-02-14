2209b0ea83fe5272ed7db72d64d22f9a8912df1f
Khai Nguyen <khainguyenstk@gmail.com>
2022-02-14 11:42:34 +0700

    Update part4 exercise bloglist to support part5

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const testingRouter = require('./controllers/testing')
 
 const app = express()
 
@@ -30,6 +31,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+if(process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
